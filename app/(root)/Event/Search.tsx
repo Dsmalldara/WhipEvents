cfa6a27cf8d5fbb2c6f3a1a02e1312ffc6819c3a
Darasimi <topedarasimi5@gmail.com>
2024-05-17 00:21:28 +0100

    optimized search query with debounce

--- app/(root)/Event/Search.tsx
+++ app/(root)/Event/Search.tsx
@@ -1,31 +1,35 @@
 'use client'
-import { searchEventsByTitle } from "@/app/api/event.actions"
 import {   Command, CommandDialog,  CommandEmpty,  CommandGroup,  CommandInput,CommandItem,CommandList,  CommandSeparator,   CommandShortcut, } from "@/components/ui/command"
-import { useEffect, useState } from "react"
-import {debounce} from "lodash"
+import { useEffect, useMemo, useState } from "react"
 import { handleError } from "@/lib/utils";
 import Link from "next/link"
+import { searchEventsByTitle } from "@/app/api/event.actions";
+import { useDebounce } from 'use-debounce';
 function Search() {
   const [searchText, setSearchText] = useState<string>("")
   const [resultText,setResultText] = useState<any>([])
-  const debouncedGetResult = debounce(async () => {
-    try {
-      if (searchText.length > 1) {
-        const result = await searchEventsByTitle(searchText);
-        setResultText(result);
-      }
-    } catch (error) {
-      // Handle error
-    }
-  }, 2000);
+  const [debouncedSearchText] = useDebounce(searchText, 3000); // Debounce search text
+
   useEffect(() => {
-    debouncedGetResult.flush(); // Clear the debounce timer
-  }, [searchText]);
-   useEffect(()=>{
-      debouncedGetResult()
-   
-   },[searchText])
-  console.log(resultText)
+    const getSearch = async () => {
+      console.log('Fetching search results...');
+      try {
+        const result = await searchEventsByTitle(debouncedSearchText); // Use debounced search text
+        setResultText(result);
+      } catch (error) {
+        handleError(error);
+      }
+    };
+
+    if (debouncedSearchText) {
+      getSearch();
+    }
+  }, [debouncedSearchText]); // Trigger effect on debounced search text
+  const renderedResultText = useMemo(() => {
+    console.log(searchText);
+    return resultText;
+  }, [resultText]);
+  // Rest of your component code
   return (
     <div className="  max-w-md w-full">
         <Command>
@@ -34,11 +38,11 @@ function Search() {
   <CommandEmpty>
   your result would be displayed here {'   '}
     </CommandEmpty>
-   {  searchText.length >0 ?(
+   {  searchText.length > 0 && resultText.length>0 ?(
     <CommandGroup heading="Suggestions">
     {
-        resultText.map((result:any)=>(
-          <Link href={`/Event/${result._id}`}>
+       renderedResultText.map((result:any)=>(
+          <Link href={`/Event/${result._id}`} key={result._id}>
           <CommandItem key={result._id} className="cursor-pointer">
          {result.title}
           </CommandItem>

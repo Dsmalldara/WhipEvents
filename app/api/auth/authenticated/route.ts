f8f5acc16607bea1bef559b5f95105a6c14a8bd6
Darasimi <topedarasimi5@gmail.com>
2024-05-25 11:03:18 +0100

    made changes to auth

--- app/api/auth/authenticated/route.ts
+++ app/api/auth/authenticated/route.ts
@@ -1,30 +1,50 @@
 import { connectToDatabase } from "@/lib/database";
 import User from "@/lib/database/models/userModel";
 import { handleError } from "@/lib/utils";
-import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextResponse } from "next/server"
- export async function GET(){
-    
-        try{
-          const {getUser} = getKindeServerSession()
-          const user = await getUser()
-          if(!user || user === null || !user.id) {throw new Error ("user not logged in")}
-            
-                await connectToDatabase()
-                const existingUser = await User.findOne({email: user.email})
-              if(!existingUser){
-                await User.create({
-                  family_name: user.family_name ?? '',
-                  given_name: user.given_name ?? '',
-                  picture: user.picture ?? 'https://avatar.vercel.sh/rauchg',
-                  email: user.email ?? '',
-                  id:user.id
-                })
-              }
-              return NextResponse.redirect('https://whipevents.vercel.app')
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+import {unstable_noStore as noStore} from "next/cache"
+export async function GET() {
+  noStore()
+  try {
+    console.log("Starting GET request handler");
 
-        }
-        catch(error){
-            handleError(error)
-        }
+    const { getUser } = getKindeServerSession();
+    if (!getUser) {
+      throw new Error("getKindeServerSession is not returning getUser function. Check environment variables.");
+    }
+
+    console.log("getKindeServerSession retrieved");
+
+    const user = await getUser();
+    console.log("User retrieved:", user);
+
+    if (!user || user === null || !user.id) {
+      console.error("User not authenticated:", user);
+      throw new Error("User not logged in");
+    }
+
+    await connectToDatabase();
+    console.log("Database connected");
+
+    const existingUser = await User.findOne({ email: user.email });
+    console.log("Existing user found:", existingUser);
+
+    if (!existingUser) {
+      await User.create({
+        family_name: user.family_name ?? '',
+        given_name: user.given_name ?? '',
+        picture: user.picture ?? 'https://avatar.vercel.sh/rauchg',
+        email: user.email ?? '',
+        id: user.id,
+      });
+      console.log("New user created");
+    }
+
+    return NextResponse.redirect('https://whipevents.vercel.app');
+  } catch (error) {
+    console.error("Error occurred:", error.message, error.stack);
+    handleError(error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
